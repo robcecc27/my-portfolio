deb6808c6aa249fcbba6bc584f08dc3a3f46ce2f
robcecc27 <rob.cecchini@gmail.com>
2018-11-24 11:28:16 +0300

    Added component to show example work in modal

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ import ExampleWork from './example-work';
 const myWork = [
   {
   'title': "AWS Polly Text to Speech",
+  'href': "https://studywithpolly.com",
+  'desc': "A text to speech applications. Enter any text in the box and click submit. Use the returned ID to retrieve the audio version of the text.",
     'image': {
       'desc': "Text to Speech using AWS Polly",
       'src': "images/polly.png",
@@ -13,17 +15,21 @@ const myWork = [
   },
   {
     'title': "Work Example",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of a project involving chemistry",
-      'src': "images/example2.png",
+      'src': "images/example1.png",
       'comment': ""
     }
   },
   {
     'title': "Work Example",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of this project",
-      'src': "images/example1.png",
+      'src': "images/example2.png",
       'comment': ""
     }
   }
